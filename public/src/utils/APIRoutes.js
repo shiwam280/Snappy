574f9f0d76d70c5fca1c84ef4edcb58ebf487ef7
shiwam singh <96126065+shiwam280@users.noreply.github.com>
2024-08-01 12:31:50 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "snappy-server.vercel.app";
+export const host = "https://snappy-server.vercel.app";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
