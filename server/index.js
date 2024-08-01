3822c7b90a59c3385ef16ce265cf937e66323328
shiwam singh <96126065+shiwam280@users.noreply.github.com>
2024-08-01 12:23:03 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "https://snappy-frontend-jaxo.onrender.com",
+    origin: "http://localhost:3000",
     credentials: true,
   },
 });
